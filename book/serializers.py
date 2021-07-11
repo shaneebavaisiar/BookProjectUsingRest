4e1786d1232df176c641bd287a4d8e54ffcabd5e
shaneeba <shaneebavaisiar007@gmail.com>
2021-07-11 12:37:07 +0530

    added

---
+++ book/serializers.py
@@ -0,0 +1,7 @@
+
+from .models import Book
+from rest_framework.serializers import ModelSerializer
+class BookSerializer(ModelSerializer):
+    class Meta:
+        model=Book
+        fields='__all__'
